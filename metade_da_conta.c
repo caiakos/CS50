d046d73e71a68c8da9aebdd508e34779e91b319e
caiakos <104407618+caiakos@users.noreply.github.com>
2024-03-23 02:03:29 -0300

    Add files via upload

---
+++ metade_da_conta.c
@@ -0,0 +1,66 @@
+/**
+ * IDENTIFICAÇÃO DO ESTUDANTE:
+ * Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+ * nenhuma linha deste comentário de seu código!
+ *
+ *    Nome completo:
+ *    Matrícula:
+ *    Turma:
+ *    Email:
+ *
+ * DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+ * Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+ * pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+ * e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+ * Estou ciente de que todo código enviado será verificado automaticamente
+ * contra plágio e que caso eu tenha praticado qualquer atividade proibida
+ * conforme as normas da disciplina, estou sujeito à penalidades conforme
+ * definidas pelo professor da disciplina e/ou instituição.
+ */
+
+
+/**
+ * Inclusão de bibliotecas:
+ */
+#include <cs50.h>
+#include <stdio.h>
+
+/**
+ * Função main
+ */
+int main(void)
+{
+    // Solicita o valor da comida:
+    float comida;
+    do
+    {
+        comida = get_float("Valor da comida: ");
+    }
+    while (comida < 10.00 || comida > 999.99);
+
+    // Solicita o percentual de impostos:
+    float impostos;
+    do
+    {
+        impostos = get_float("Porcentagem de impostos: ");
+    }
+    while (impostos < 0.00 || impostos > 100.00);
+
+    // Solicita o percentual de gorjeta:
+    int gorjeta;
+    do
+    {
+        gorjeta = get_int("Porcentagem da gorjeta: ");
+    }
+    while (gorjeta < 0 || gorjeta > 100);
+
+    // TODO: coloque aqui seu código para o cálculo do valor da conta.
+    // Não se esqueça de imprimir o valor final conforme especificado na
+    // descrição do exercício.
+    float total = comida * (1 + impostos * 0.01) * (1 + gorjeta * 0.01);
+    {
+        printf("Cada um pagará R$ %.2f!\n", total / 2);
+    }
+    // Termina o programa:
+    return 0;
+}
